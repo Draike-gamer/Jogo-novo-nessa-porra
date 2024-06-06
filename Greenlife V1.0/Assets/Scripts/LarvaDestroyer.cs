db652fec74c6249a787b9259bb9a78aa93fd213d
Draike-gamer <64713553+Draike-gamer@users.noreply.github.com>
2024-06-05 22:34:28 -0300

    update

--- Greenlife V1.0/Assets/Scripts/LarvaDestroyer.cs
+++ Greenlife V1.0/Assets/Scripts/LarvaDestroyer.cs
@@ -8,11 +8,15 @@ public class LarvaDestroyer : MonoBehaviour
     public GameObject painelAtivado;
 
     public bool isDestroyParticleSystemActive = false;
+    public float destroyDelay = 2.0f; // Tempo de atraso em segundos
+    private int larvasDestruidas = 0;
+    private const int maxLarvasDestruidas = 15;
 
     void Start()
     {
         destroyParticleSystem.Stop();
         isDestroyParticleSystemActive = false;
+        painelAtivado.SetActive(false); // Certifique-se de que o painel esteja desativado no início
     }
 
     void Update()
@@ -36,9 +40,8 @@ public class LarvaDestroyer : MonoBehaviour
             {
                 if (hit.collider.CompareTag("Larva"))
                 {
-                    Debug.Log("Larva detected and destroyed: " + hit.collider.name);
-                    Destroy(hit.collider.gameObject);
-                    GlobalManager.Instance.IncrementarLarvasDestruidas(); // Incrementa o contador no GlobalManager
+                    Debug.Log("Larva detected: " + hit.collider.name);
+                    StartCoroutine(DestroyLarvaAfterDelay(hit.collider.gameObject, destroyDelay));
                 }
             }
         }
@@ -56,5 +59,35 @@ public class LarvaDestroyer : MonoBehaviour
         destroyParticleSystem.Stop();
         isDestroyParticleSystemActive = false;
         Debug.Log("Destroy particle system deactivated.");
+    }
+
+    private IEnumerator DestroyLarvaAfterDelay(GameObject larva, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        if (isDestroyParticleSystemActive)
+        {
+            Debug.Log("Larva destroyed: " + larva.name);
+            Destroy(larva);
+            IncrementarLarvasDestruidas();
+        }
+    }
+
+    private void IncrementarLarvasDestruidas()
+    {
+        larvasDestruidas++;
+        GlobalManager.Instance.IncrementarLarvasDestruidas(); // Incrementa o contador no GlobalManager
+
+        if (larvasDestruidas >= maxLarvasDestruidas)
+        {
+            AtivarPainel();
+        }
+
+        Debug.Log("Larvas destruídas: " + larvasDestruidas);
+    }
+
+    private void AtivarPainel()
+    {
+        painelAtivado.SetActive(true);
+        Debug.Log("Painel ativado.");
     }
 }
